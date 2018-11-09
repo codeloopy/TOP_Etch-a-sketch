399cd5e4f6353188f8fadf44823d927b13a57c1b
Pedro <codeloopy@gmail.com>
2018-11-08 21:28:05 -0500

    created the initial 16 squares via JS

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+for (var i = 0; i <= 16; i++) {
+  var div = document.createElement("div");
+  div.className = "squares";
+  document.querySelector(".boardGame").appendChild(div);
+  // document.querySelector('.boardGame').appendChild('div');
+}
